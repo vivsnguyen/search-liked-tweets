d345291a93fa59ee375cba1d289c3f18b5865a70
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-09-18 15:51:40 -0700

    add search, deebug get_liked_list

--- twitter_api.py
+++ twitter_api.py
@@ -1,1 +1,20 @@
+import os
 import requests
+
+header_info = {
+    'oauth_consumer_key': os.environ['TWITTER_API_KEY'],
+    'oauth_consumer_secret': os.environ['TWITTER_API_SECRET_KEY']
+}
+
+dane_user = 'danusinmyanus'
+
+def get_liked_list(screen_name):
+    """
+    """
+    url = 'https://api.twitter.com/1.1/favorites/list.json'
+
+    params_info = {'screen_name': dane_user, 'count': 5}
+
+    response = requests.get(url,params=params_info,headers=header_info).json()
+    
+    return response
