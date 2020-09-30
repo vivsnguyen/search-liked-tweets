3a816d93eb6a29cafb00c538851924f28e29842e
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-09-30 14:46:11 -0700

    add tweepy oauth

--- twitter_api.py
+++ twitter_api.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import, print_function
+import tweepy
+
 import os
 import requests
 
@@ -10,16 +13,50 @@ header_info = {
 
 dane_user = 'danusinmyanus'
 
+# == OAuth Authentication ==
+#
+# This mode of authentication is the new preferred way
+# of authenticating with Twitter.
+
+# The consumer keys can be found on your application's Details
+# page located at https://dev.twitter.com/apps (under "OAuth settings")
+consumer_key = os.environ['TWITTER_API_KEY']
+consumer_secret = os.environ['TWITTER_API_KEY_SECRET']
+
+# The access tokens can be found on your applications's Details
+# page located at https://dev.twitter.com/apps (located
+# under "Your access token")
+access_token = os.environ['TWITTER_ACCESS_TOKEN']
+access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# If the authentication was successful, you should
+# see the name of the account print out
+print(api.me().name)
+
+# If the application settings are set for "Read and Write" then
+# this line should tweet out the message to your account's
+# timeline. The "Read and Write" setting is on https://dev.twitter.com/apps
+api.update_status(status='Updating using OAuth authentication via Tweepy!')
+
+
 def get_liked_list(screen_name):
     """
     """
+
     url = 'https://api.twitter.com/1.1/favorites/list.json'
 
-    params_info = {'screen_name': dane_user, 'count': 5}
+    params_info = {'screen_name': screen_name, 'count': 5}
 
     response = requests.get(url,params=params_info,headers=header_info).json()
     
     return response
 
 def search():
-    ""
+    """
+    """
+    pass
